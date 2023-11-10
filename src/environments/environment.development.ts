057ef0ac815c1021e93fbdb4a2c5d4b28c40c264
Jonas Dankertsen <jhdank@gmail.com>
2023-11-10 10:28:16 +0100

    environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://eeqjlsgheqkhvsogitsu.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlcWpsc2doZXFraHZzb2dpdHN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg5OTkwNDUsImV4cCI6MjAxNDU3NTA0NX0.ws9hr78TYp0C3aVs0VnBot8RTs3IK8f4JP1_N4iC9nY',
+};
